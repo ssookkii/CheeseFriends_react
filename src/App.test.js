679f641cb5ac4c0a93af074fe460254495db52b6
ssookkii <sangsook4702@gmail.com>
2023-04-10 16:24:55 +0900

    test1

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 //merge 테스트
 //test//
+//test//
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
