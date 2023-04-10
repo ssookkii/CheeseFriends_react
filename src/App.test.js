bf42677c0b316e479d102958e601cca180c9802e
kwonss22 <young.eee920@gmail.com>
2023-04-10 19:06:19 +0900

    .

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,8 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 //merge 테스트
 //test//
-//test//
+//test// 소영님 화이팅,,,,
+//vscode
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
