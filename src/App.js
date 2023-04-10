23eec5356f4fdd0d9051c927458672fef147406b
onlyoy <onlyoy4@naver.com>
2023-04-10 16:21:15 +0900

    psj test2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      psj 분기함
+   
      
     </div>
   );
--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-//merge 테스트
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
