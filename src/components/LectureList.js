9efe7e7d2a2e7643324358870b6cc9c480971e14
jyeonshin <94603624+jyeonshin@users.noreply.github.com>
2023-04-16 00:08:20 +0900

    부트스트랩 추가 및 강의 목록 페이지 작성, 추가작업 필요

---
+++ src/components/LectureList.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import axios from "axios";
+
+function LectureList() {
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default LectureList;
