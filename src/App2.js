5d372c2c5c26aaccea16a47f297613a3b475f813
ssookkii <sangsook4702@gmail.com>
2023-04-19 20:52:26 +0900

    0419

--- src/App2.js
+++ src/App2.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Attendance from "./components/Attendance";
+import AttendanceManage from "./components/AttendanceManage";
 import Test from "./components/Test";
 import './App.css';
 import Login from './login/login';
@@ -22,6 +23,7 @@ function App() {
                 <Route path="/regi" element={<Regi/>}></Route>
 
                 <Route path="/attendance" element={<Attendance />} />
+                <Route path="/AttendanceManage" element={<AttendanceManage />} />
                 <Route path="/" element={<Test />} />
               </Routes>
             </div>
