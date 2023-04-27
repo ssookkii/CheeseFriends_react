85c72b6df07bbe9f9f9dd90cff773cab0b3ef234
jyeonshin <94603624+jyeonshin@users.noreply.github.com>
2023-04-27 15:14:41 +0900

    04-27

---
+++ src/components/PapagoTranslator.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+function PapagoTranslator() {
+  const [text, setText] = useState("");
+  const [translatedText, setTranslatedText] = useState("");
+
+  const translate = () => {
+    const clientId = "9sBjPxbXT3NFK1O86020";
+    const clientSecret = "zlm7AAxGhe";
+    const source = "ko";
+    const target = "en";
+    const apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+
+    fetch(apiURL, {
+      mode: 'cors',
+      credentials: 'include',
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "X-Naver-Client-Id": clientId,
+        "X-Naver-Client-Secret": clientSecret,
+      },
+      body: `source=${source}&target=${target}&text=${encodeURIComponent(text)}`,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setTranslatedText(data.message.result.translatedText);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={translate}>번역</button>
+      <p>{translatedText}</p>
+    </div>
+  );
+}
+
+export default PapagoTranslator;
