be0b8c14da173c8bc8d262607f71a7bb2b9df393
ssookkii <sangsook4702@gmail.com>
2023-04-27 09:46:39 +0900

    app.js

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import TeaUpdate from './components/TeaUpdate';
 import SendMailManage from './components/SendMailManage';
 import MailWrite from './components/MailWrite';
 import QnAManage from './components/QnAManage';
+import QnaAnswer from './components/QnaAnswer';
 import GradeManage from './components/GradeManage';
 import Attendance from "./components/Attendance";
 import AttendanceQR from "./components/AttendanceQR";
@@ -35,8 +36,12 @@ import RegiParents from './login/regiparents';
 import Regiselect from './login/regiselect';
 import Regiteacher from './login/regiteacher';
 import Changeme from './mypage/changeme';
+import Email from './mypage/receiveemaillist';
 import Sendemail from './mypage/sendemail';
 import Testmain from './mypage/testmain';
+import Maildetail from './mypage/maildetail';
+import Sendemaillist from './mypage/sendemaillist';
+import Sendmaildetail from './mypage/sendmaildetail';
 
 import LectureList from './components/LectureList'
 import AbLectureList from './components/AbLectureList'
@@ -55,6 +60,11 @@ import QnaLearningAnswer from './components/QnaLearningAnswer';
 import EduInfoList from './components/EduInfoList';
 import EduInfoDetail from './components/EduInfoDetail';
 import EduInfoWrite from './components/EduInfoWrite';
+import Chatbot from './components/Chatbot';
+
+import PlayGame from './components/PlayGame';
+import PlayGame1 from './components/PlayGame1';
+import PlayGame2 from './components/PlayGame2';
 
 function App() {
 
@@ -64,6 +74,12 @@ function App() {
 
         <main>
           <Routes>
+
+            <Route path="/playgame" element={<PlayGame />} />
+            <Route path="/playgame1" element={<PlayGame1 />} />
+            <Route path="/playgame2" element={<PlayGame2 />} />
+
+            <Route path="/service/Chatbot" element={<Chatbot />} />
 
             <Route path="/lecture" element={<LectureList />} />
             <Route path="/lecture/AbLectureList" element={<AbLectureList />} />
@@ -93,7 +109,14 @@ function App() {
             <Route path="/passwordsearch" element={<Passwordsearch />} />
             <Route path="/testmain" element={<Testmain />}>
               <Route path="/testmain/changeme" element={<Changeme />} />
+              <Route path="/testmain/email" element={<Email />} />
+              <Route path="/testmain/email/:choice/:search" element={<Email />} />
               <Route path="/testmain/sendemail" element={<Sendemail />} />
+              <Route path="/testmain/sendemail/:sender" element={<Sendemail />} />
+              <Route path="/testmain/maildetail/:seq" element={<Maildetail />} />
+              <Route path="/testmain/sendemaillist" element={<Sendemaillist />} />
+              <Route path="/testmain/sendemaillist/:choice/:search" element={<Sendemaillist />} />
+              <Route path="/testmain/sendmaildetail/:seq" element={<Sendmaildetail />} />
             </Route>
 
 
@@ -121,6 +144,7 @@ function App() {
                 <Route path="/adminpage/sendmailmanage" exact element={<SendMailManage />} />
                 <Route path="/adminpage/mailwrite" exact element={<MailWrite />} />
                 <Route path="/adminpage/qnamanage" exact element={<QnAManage />} />
+                <Route path="/adminpage/qnaanswer/:seq" exact element={<QnaAnswer />} />
 
               </Route>
             </Route>
