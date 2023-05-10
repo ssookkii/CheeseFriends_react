4a73ffb960adeabba75137532c7199e5e428b7df
Nedory97 <gusrkfka01sky@gmail.com>
2023-05-10 14:17:35 +0900

    CSS 변경

--- src/components/PlayGame.js
+++ src/components/PlayGame.js
@@ -14,9 +14,9 @@ function PlayGame() {
 
   return (
 
-    <div>
-      <div className="divCenter">
-        <h1>Prop 2 Sky</h1>
+    <div style={{width:1000}}>
+      <div className="divCenter" style={{marginTop:40}}>
+        <h1><b>Prop 2 Sky</b></h1>
       </div><br/><br/>
 
       <div className="divCenter">
@@ -25,34 +25,38 @@ function PlayGame() {
       <div className="divCenter">
       {showIframe && <button className="btn" type="button" onClick={handlePlayGame}>게임 설명</button>}<br/><br/>
       </div>
-      {showIframe && <div><iframe src="../WebGLTest/index.html" width="520" height="1090"></iframe></div>}<br/><br/>
+      {showIframe && <div className="divCenter"><iframe src="../WebGLTest/index.html" width="520" height="1090"></iframe></div>}<br/><br/>
 
       {showGameexp && <div className="divCenter">
-        <h1>게임 설명</h1><br/>
+        <h1 style={{marginBottom:100}}><b>게임 설명</b></h1><br/><br/>
         <h2>1. 게임 시작!</h2><br/>
-        <img src="/img/GameStartImg.png" alt="게임 설명 이미지" width="200" height="auto" />
-        <p>'Start' 버튼을 누르면 게임이 시작되며 캐릭터가 비행을시작합니다.</p><br/>
         <video muted autoPlay loop width="200" height="auto">
           <source src="/img/game_1.mp4" type="video/mp4"/>
         </video>
-        <p>마우스를 클릭하여 비행 방향을 '좌','우'로 바꿀 수 있습니다.</p><br/><br/>
+        <p style={{marginTop : 20}}>'Start' 버튼을 누르면 게임이 시작되며 캐릭터가 비행을시작합니다.</p>
+        <p style={{marginTop : 20}}>마우스를 클릭하여 비행 방향을 '좌','우'로 바꿀 수 있습니다.</p><br/><br/>
 
-        <h2>2. 장애물을 통과하여 점수를 획득하세요!</h2><br/>
-        <video muted autoPlay loop width="200" height="auto">
-          <source src="/img/game_1.mp4" type="video/mp4"/>
-        </video>
-        <p>장애물에 부딪히면 캐릭터가 추락하며 게임이 종료됩니다.</p><br/>
+        <h2>2. 장애물을 통과하여 점수를 획득하세요!</h2><br/><br/>
+        <div style={{float:"left", width:500}}>
+          <video muted autoPlay loop width="200" height="auto">
+            <source src="/img/game_1.mp4" type="video/mp4"/>
+          </video>
+        <p style={{marginTop : 20}}>장애물에 부딪히면 캐릭터가 추락하며 게임이 종료됩니다.</p><br/>
+        </div>
+        <div style={{float:"right", width:500}}>
         <video muted autoPlay loop width="200" height="auto">
           <source src="/img/game_2.mp4" type="video/mp4"/>
         </video>
-        <p>또한 게임 화면 좌측, 우측 경계에 부딪혀도 게임이 종료됩니다.</p><br/><br/>
-
-        <h2>3. 더 높이 날기 위해 <img src="/img/battery.png" width="30" height="30" /> (배터리) 를 챙기세요!</h2><br/>
+        <p style={{marginTop : 26}}>게임 화면 좌측, 우측 경계에 부딪혀도 게임이 종료됩니다.</p><br/><br/>
+        </div><br/>
+        <div style={{float:"left", width:1000}}>
+        <h2>3. 더 높이 날기 위해 <img src="/img/battery.png" width="30" height="30" /> (배터리) 를 챙기세요!</h2><br/><br/>
         <video muted autoPlay loop width="200" height="auto">
           <source src="/img/game_3.mp4" type="video/mp4"/>
         </video>
-        <p>프로펠러 헬멧의 에너지가 모두 소모되면 캐릭터가 추락합니다.</p>
+        <p style={{marginTop : 20}}>프로펠러 헬멧의 에너지가 모두 소모되면 캐릭터가 추락합니다.</p>
         <p>화면 상단의 에너지를 확인하고 배터리를 챙겨주세요.</p><br/><br/>
+        </div>
       </div>}
     </div>
   );
--- src/components/PlayGame2.js
+++ src/components/PlayGame2.js
@@ -15,8 +15,8 @@ function PlayGame2() {
   return (
 
     <div>
-       <div className="divCenter">
-            <h1>Speed Typing</h1>
+       <div className="divCenter" style={{margin:40}}>
+            <h1><b>Speed Typing</b></h1>
        </div><br/><br/>
 
        <div className="divCenter">
@@ -25,18 +25,19 @@ function PlayGame2() {
        <div className="divCenter">
         {showIframe && <button className="btn" type="button" onClick={handlePlayGame}>게임 설명</button>}<br/><br/>
        </div>
-        {showIframe && <div><iframe src="../typingGame/index.html" width="800" height="800"></iframe></div>}<br/><br/>
+        {showIframe && <div style={{marginTop:80}}><iframe src="../typingGame/index.html" width="800" height="800"></iframe></div>}<br/><br/>
 
       {showGameexp && <div className="divCenter">
-        <h1>게임 설명</h1><br/>
+        <h1 style={{marginBottom:100}}><b>게임 설명</b></h1><br/>
         <h2>1. 게임 시작!</h2><br/>
         <p><b>Speed Typing</b> 은 제시어를 제한시간 안에 빠르게 입력하는 게임입니다.</p><br/>
-        <img src="/img/SpeedTypingImg1.png" alt="게임 설명 이미지" width="500" height="auto" />
+        <img src="/img/SpeedTypingImg1.png" alt="게임 설명 이미지" width="500" height="auto" /><br/><br/>
         <p>게임이 시작되면 제시어와 함께 좌측에는 제한시간, 우측에는 점수가 보여집니다.</p><br/><br/>
 
-        <h2>2. 난이도를 선택할 수 있어요!</h2>
-        <img src="/img/SpeedTypingImg2.png" alt="게임 설명 이미지" width="500" height="auto" />
-        <p>화면 상단에서 'Easy', 'Medium', 'Hard' 총 세 단계의 난이도 선택이 가능합니다..</p><br/><br/>
+        <h2>2. 난이도를 선택할 수 있어요!</h2><br/>
+        <img src="/img/SpeedTypingImg2.png" alt="게임 설명 이미지" width="500" height="auto" /><br/><br/>
+        <p>화면 상단에서 'Easy', 'Medium', 'Hard' 총 세 단계의 난이도 선택이 가능합니다.</p>
+        <p>자신에게 적당한 난이도를 선택하여 게임을 즐겨보세요.</p><br/><br/>
       </div>}
     </div>
   );
